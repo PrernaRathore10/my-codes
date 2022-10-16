e81ff8777c11e082a160c48b6ce898291696ad79
PrernaRathore10 <113295862+PrernaRathore10@users.noreply.github.com>
2022-10-16 19:18:48 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,9 +3,13 @@
 def bubbleSort(arr):
     n = len(arr)
     for i in range(0, n):
+        swapped=False
         for j in range(i + 1, n):
             # swapping condition
             if arr[i] > arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
+                swapped=True
             else:
                 pass
+        if(swapped==False):
+            break
